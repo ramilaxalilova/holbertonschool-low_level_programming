1bb84937f871025576475de50f5901b31f8fe101
ramilaxalilova <xalilovaramila@yahoo.com>
2023-10-16 04:52:10 -0700

    print all hexadecimals

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - prints numbers of base 16 in lowercase
+*Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+	{
+		printf("%x", num);
+	}
+	printf("\n");
+	return (0);
+}

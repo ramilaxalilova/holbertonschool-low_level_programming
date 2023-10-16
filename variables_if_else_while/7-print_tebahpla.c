b20a9406c16c8b8d3b6dc8531eb10c7d53844223
ramilaxalilova <xalilovaramila@yahoo.com>
2023-10-16 04:44:12 -0700

    alphabet reverse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-	char letter;
+	char letter = 'z';
 
-	for (letter = 'z'; letter <= 'a'; letter++)
+	while  (letter >= 'a')
+	{
 		putchar(letter);
-
+		letter--;
+	}
 	putchar('\n');
 
 	return (0);

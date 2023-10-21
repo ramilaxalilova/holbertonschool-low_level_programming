9f9c472817b7d7f8c35a33539d8dc636f3e5577f
ramilaxalilova <xalilovaramila@yahoo.com>
2023-10-21 11:43:10 -0700

    not fizz but buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 
 /**
  * main -  prints the numbers from 1 to 100 with fizzbuz
- * Return: numbers with fizzbuzz
+ * Return: Always 0
  *
  */
 
@@ -15,17 +15,21 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 
 		else if (n % 3 == 0)
 
-			printf("Fizz ");
+			printf("Fizz");
 
 		else if (n % 5 == 0)
 
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", n);
+			printf("%d", n);
+		if (n == 100)
+			continue;
+		printf(" ");
+
 	}
 	printf("\n");
 	return (0);

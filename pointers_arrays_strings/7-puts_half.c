199e1f15e3a3ea84688230e8a6d183552f6e9671
ramilaxalilova <xalilovaramila@yahoo.com>
2023-10-24 00:50:14 -0700

    putshalf

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * puts_half - prints half of a string
+ * @str: string that will be reversed
+ */
+
+void puts_half(char *str)
+{
+	int i, len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = len / 2; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

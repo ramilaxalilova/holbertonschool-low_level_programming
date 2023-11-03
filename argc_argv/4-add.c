e63f65272fe3a50115dfbf67facc3877fd2372ca
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-03 11:44:45 -0700

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * isNumber - check string is number or not
+ * @str: string
+ * Return: true false
+ */
+
+
+int isNumber(char *str)
+{
+	char *endptr;
+	strtol(str, &endptr, 10);
+	return (*endptr == '\0');
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: result
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+	int i;
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isNumber(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}

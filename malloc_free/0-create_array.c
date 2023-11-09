3d293694c2c356bc375a7e5505002b10c9fb35c4
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-09 02:04:49 -0800

    0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - creates array
+ * @size: size of array
+ * @c: chars of array
+ * Return: array
+ */
+
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		a[i] = c;
+
+	return (a);
+}

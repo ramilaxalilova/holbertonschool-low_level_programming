ca6db4e5d96f30deb3dd173311531b002a6928af
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-13 04:59:00 -0800

    alloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@
 
 int **alloc_grid(int height, int width)
 {
-	int i, j;
+	int i, j, k, l;
 
 	int **arr;
 
@@ -20,20 +20,30 @@ int **alloc_grid(int height, int width)
 		return (NULL);
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 		{
-			free(arr);
-			for (j = 0; j <= height; j++)
+			for (j = i; j >= 0; j--)
+			{
 				free(arr[j]);
+			}
+			free(arr);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
-			arr[i][j] = 0;
+	}
+	for (k = 0; k < height; k++)
+	{
+		for (l = 0; l < width; l++)
+		{
+			arr[k][l] = 0;
+		}
 	}
 	return (arr);
 }

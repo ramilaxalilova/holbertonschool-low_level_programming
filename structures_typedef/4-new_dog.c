a50134092536e4d58469636f55fc80a4ecc3d775
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-21 01:50:47 -0800

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -23,19 +23,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
+	{
+		free(doggy);
 		return (NULL);
+	}
 
 	doggy->name = malloc(nlen * sizeof(doggy->name));
-	if (doggy == NULL)
+	if (doggy->name == NULL)
+	{
+		free(doggy);
 		return (NULL);
+	}
 	for (i = 0; i < nlen; i++)
 		doggy->name[i] = name[i];
 
 	doggy->age = age;
 
 	doggy->owner = malloc(olen * sizeof(doggy->owner));
-	if (doggy == NULL)
+	if (doggy->owner == NULL)
+	{
+		free(doggy->name);
+		free(doggy);
 		return (NULL);
+	}
 	for (i = 0; i < olen; i++)
 		doggy->owner[i] = owner[i];
 	return (doggy);

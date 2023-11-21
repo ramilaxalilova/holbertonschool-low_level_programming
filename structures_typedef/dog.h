f15063c5cf1966b46169115cad2fc5fb2f84534c
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-20 23:16:43 -0800

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's properties
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

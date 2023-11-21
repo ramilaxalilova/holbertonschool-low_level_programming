618b9d33f94e816ae3c8897fba18636603f1c46b
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-21 01:44:11 -0800

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age of new dog
+ * @owner: owner of new dog
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int nlen, olen, i;
+	dog_t *doggy;
+
+	nlen = olen = 0;
+
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+
+	doggy = malloc(sizeof(dog_t));
+	if (doggy == NULL)
+		return (NULL);
+
+	doggy->name = malloc(nlen * sizeof(doggy->name));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
+
+	doggy->age = age;
+
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
+	if (doggy == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
+	return (doggy);
+}

d8c6f9603192931461d8e160c2769bf5d30423d2
ramilaxalilova <xalilovaramila@yahoo.com>
2023-11-21 00:24:38 -0800

    poppy

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+#include "math.h"
+/**
+ * print_dog - prints dog values
+ * @d: dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
